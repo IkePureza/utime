95b31e1823e3f2d6c97701ce62848d491d1f2e00
zlatax <keigonagai@gmail.com>
2022-09-06 11:20:09 +1000

    fix: fixes from code review

--- app/components/newHouseForm.tsx
+++ app/components/newHouseForm.tsx
@@ -1,32 +1,45 @@
 import React from 'react'
 
 interface NewHouseFormProps {
-    handleClick: (event:any) => Promise<void>
+    handleClick: (event: any) => Promise<void>
 }
 
 function NewHouseForm(props: NewHouseFormProps) {
-
-    
     return (
         <form onSubmit={props.handleClick} action="#">
             <div className="form-control w-full max-w-xs">
                 <label className="label">
                     <span className="label-text">House Name</span>
                     <span className="label-text-alt">(Required)</span>
-                </label> 
-                <input id="house_name" name="house_name" type="input w-full max-w-xs" placeholder="House Name" className="input input-bordered" />
+                </label>
+                <input
+                    id="houseName"
+                    name="houseName"
+                    type="input w-full max-w-xs"
+                    placeholder="House Name"
+                    className="input input-bordered"
+                />
             </div>
-            <br/>
+            <br />
             <div className="form-control">
                 <label className="label">
                     <span className="label-text">Description (Optional)</span>
                     <span className="label-text-alt">Maximum 50 words</span>
-                </label> 
-                <textarea id="house_desc" name="house_desc" className="textarea textarea-bordered h-24" placeholder="Description"></textarea> 
+                </label>
+                <textarea
+                    id="house_desc"
+                    name="house_desc"
+                    className="textarea textarea-bordered h-24"
+                    placeholder="Description"
+                ></textarea>
             </div>
-            <br/>
+            <br />
             <div className="min-w-full flex place-content-center">
-                <input className="btn" type="submit" value="create house"></input>  
+                <input
+                    className="btn"
+                    type="submit"
+                    value="create house"
+                ></input>
             </div>
         </form>
     )
