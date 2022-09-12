16d5ecb6b0016ea8a55206326a743c483bb2b0ff
Henrique Pureza <henrique.pureza13@gmail.com>
2022-09-12 14:26:57 +1000

    fix: new house form

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -33,28 +33,10 @@ function Index() {
     const houseName = event.target.elements.houseName;
     const houseDesc = event.target.elements.houseDesc;
 
-    var flag = false;
-    // Input Validation
-    if (houseName.value === "") {
-      houseName.classList.add("input-error");
-      console.log("INPUT_ERROR: House name is empty!");
-      flag = true;
-    }
-    if (houseDesc.value.length > 50) {
-      houseDesc.classList.add("textarea-error");
-      console.log("INPUT_ERROR: Description must be less than 50 characters!");
-      flag = true;
-    }
-
-    if (flag) {
-      return;
-    }
-
     const docRef = await addDoc(collection(db, "household"), {
       name: houseName.value,
       users: [userData?.userId], //need to change this bit to match the form inputs
     });
-    console.log("Document written with ID: ", docRef.id);
 
     if (modalCheckboxRef.current !== null) {
       modalCheckboxRef.current.checked = !modalCheckboxRef.current.checked;
