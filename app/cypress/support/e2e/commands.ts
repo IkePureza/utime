cb02341923e8e5a6972e801ab2b076fc42fb107c
zlatax <keigonagai@gmail.com>
2022-09-19 14:55:59 +1000

    ci: attempting to connect emulated DB

--- app/cypress/support/e2e/commands.ts
+++ app/cypress/support/e2e/commands.ts
@@ -16,4 +16,19 @@ const fbConfig = {
 
 firebase.initializeApp(fbConfig);
 
+// Emulate Firestore if Env variable is passed
+const firestoreEmulatorHost = Cypress.env("FIRESTORE_EMULATOR_HOST");
+if (firestoreEmulatorHost) {
+  firebase.firestore().settings({
+    host: firestoreEmulatorHost,
+    ssl: false,
+  });
+}
+
+const authEmulatorHost = Cypress.env("FIREBASE_AUTH_EMULATOR_HOST");
+if (authEmulatorHost) {
+  firebase.auth().useEmulator(`http://${authEmulatorHost}/`);
+  console.debug(`Using Auth emulator: http://${authEmulatorHost}/`);
+}
+
 attachCustomCommands({ Cypress, cy, firebase });
