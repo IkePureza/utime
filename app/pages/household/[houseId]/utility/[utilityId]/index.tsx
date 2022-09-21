c63c5bb4f9e9693c44f3ea885fcff92361922b6c
yangyueedson <yangyueedson@student.unimelb.edu.au>
2022-09-21 15:13:46 +1000

    feat: checks if bookings are valid (no clash + valid dates), no display on UI, only console log

--- app/pages/household/[houseId]/utility/[utilityId]/index.tsx
+++ app/pages/household/[houseId]/utility/[utilityId]/index.tsx
@@ -30,23 +30,30 @@ const Login = () => {
   const [amenityData, loading, error, snapshot] = useDocumentData(amenityRef);
 
   const handleBooking = async (event: any) => {
+    // Prevents page from auto reloading every form submit
     event.preventDefault();
     const bookingDesc = event.target.elements.desc.value;
     const bookingTo = event.target.elements.to.value;
     const bookingFrom = event.target.elements.from.value;
-
     const bookingRef = collection(db, "booking");
 
-    // Call checks here
+    // Checks if amenity is already booked
+    var validBooking = isAmenityBooked(bookingFrom, bookingTo, amenityData);
+    console.log("Is current booking valid? ", validBooking);
 
-    console.log(bookingDesc, bookingTo, bookingFrom);
+    // Checks if user is booking into the past
+    var bookingMakesSense = datesWellFormed(bookingFrom, bookingTo);
 
+    // Not sure what to do if ((bookingMakesSense || validBooking) == false)
+    // How to change UI to ask users to book new date
+
+    // Do below if ((bookingMakesSense && validBooking) == true)
     const docRef = await addDoc(bookingRef, {
       utilityId,
       desc: bookingDesc,
       userId: currentUser?.userId,
       householdId: houseId,
-      from: bookingFrom, //
+      from: bookingFrom,
       to: bookingTo,
     });
 
@@ -62,44 +69,62 @@ const Login = () => {
 
   // Checks if utility is currently booked
   // Current booking date is equal or less to the latest booking toDate
-  // const isAmenityBooked = (bookingFrom: any, bookingTo: any, amenityData: DocumentData): boolean => {
+  const isAmenityBooked = (
+    bookingFrom: any,
+    bookingTo: any,
+    amenityData: DocumentData | undefined
+  ): boolean => {
+    // if amenityData is undefined (no amenity?) then return true as no prev booking
+    if (amenityData == undefined) {
+      return true;
+    }
 
-  //   // get latestBookingFrom and latestBookingTo from UtilityID
-  //   const LBFrom = new Date(amenityData.latestBooking.from);
-  //   const LBTo = new Date(amenityData.latestBooking.to);
+    // get latestBookingFrom and latestBookingTo from UtilityID
+    const LBFrom = new Date(amenityData.latestBooking.from);
+    const LBTo = new Date(amenityData.latestBooking.to);
+    const BFrom = new Date(bookingFrom);
+    const BTo = new Date(bookingTo);
 
-  //   if (bookingFrom < LBFrom){
-  //     return false;
-  //   }
+    // Checks
+    console.log("LastBookingFrom:", LBFrom);
+    console.log("LastBookingTo", LBTo);
+    console.log("BookingFrom:", BFrom);
+    console.log("BookingTo:", BTo);
 
-  //   if (bookingFrom){
+    // If BookingFrom is > LastBookingTo
+    if (BFrom > LBTo) {
+      return true;
+    }
 
-  //   }
+    // Else anything else return false
+    return false;
+  };
 
-  //   if (currBooking.getDay() <= booking?.getDay()) {
-  //     return true;
-  //   }
-  //   return false;
-  // };
-
-  // 1. Check if theres clash in bookings
-  //  - Current booking is not within the latest known booking
-
-  //  2. Check the booking dates make sense
-  //  - From > RN (Can't book in the past)
+  //  Check the booking dates make sense
+  //  - BFrom > RN (Can't book in the past)
   //  - From-date can't be < to-date
-  // const datesWellFormed = (data: DocumentData): boolean => {
-  //   const now = new Date();
+  const datesWellFormed = (bookingFrom: any, bookingTo: any): boolean => {
+    const now = new Date(Date.now());
+    const BFrom = new Date(bookingFrom);
+    const BTo = new Date(bookingTo);
 
-  // };
+    if (BFrom > now) {
+      return true;
+    }
+
+    console.log("User is trying to book into the past");
+    return false;
+  };
 
   return (
     <>
       <NavBar></NavBar>
       <div className="flex flex-row justify-center items-center">
-        <h1 className="text-center font-black text-8xl mb-2">{value?.type}</h1>
+        <h1 className="text-center font-black text-8xl mb-2">
+          {amenityData?.type}
+        </h1>
         <h1 className="text-center font-black text-2xl mb-2 mr-1">
-          {value?.name}
+          {amenityData?.name}
         </h1>
         <div className="dropdown dropdown-right">
           <label tabIndex={0} className="btn btn-circle btn-ghost btn-xs">
@@ -136,7 +161,9 @@ const Login = () => {
       </div>
       <div className="flex flex-col justify-center items-center">
         <h2 className="text-lg text-2xl text-center text-bo">Description</h2>
-        <p className="text-center mb-8">{value?.desc ?? "No Description"}</p>
+        <p className="text-center mb-8">
+          {amenityData?.desc ?? "No Description"}
+        </p>
         <UtilityBookingForm handleSubmit={handleBooking}></UtilityBookingForm>
       </div>
     </>
