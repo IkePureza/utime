7cb4dadfc81b05ecaffe8a24b4c25784d1e53402
geordanbennie <geordanbennie@gmail.com>
2022-10-23 18:20:38 +1100

    made sign in and log in h1

--- app/pages/signUp.tsx
+++ app/pages/signUp.tsx
@@ -20,7 +20,7 @@ const Signup = () => {
     return (
       <div className="flex-col content-center justify-center px-10 py-5 shadow-lg w-96 mx-auto mt-6 mb-4">
         <img src="./logo.png" alt="..." className="justify-center px-7"/>
-        <h3 className="text-center text-3xl mb-2">Sign Up</h3>
+        <h1 className="text-center text-3xl font-normal mb-2">Sign Up</h1>
         <div className="flex flex-col gap-y-1">
           <EmailPasswordAuthSignUp />
           <div className="divider text-sm mb-0">OR</div>
