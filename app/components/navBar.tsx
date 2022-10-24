a3fbd9e310185fb5aa6190e5788e89fc8779d894
geordanbennie <geordanbennie@gmail.com>
2022-10-24 17:23:39 +1100

    added hamburger back to nav bar

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -31,7 +31,22 @@ const NavBar = () => {
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost h-max rounded-2xl w-max">
             
-            <div className="w-11">{/*make user profile pic*/}
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8 mr-1"
+              id="hamburger"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+            <div className="w-11">
               <Image
                 className="rounded-full avatar"
                 src="https://placeimg.com/80/80/people"
