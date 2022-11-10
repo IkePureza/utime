4d67bb7cf4d6374120b4d0191135b76aae78081d
geordanbennie <geordanbennie@gmail.com>
2022-11-10 19:25:00 +1100

    Edited e2e household test to suit design

--- app/cypress/e2e/tests/02_household.cy.ts
+++ app/cypress/e2e/tests/02_household.cy.ts
@@ -20,7 +20,7 @@ describe("User Household Tests", () => {
     before(() => {
       cy.visit("/");
       cy.get(".card-actions > .btn").click();
-      cy.contains("Household testHouse");
+      cy.contains("testHouse");
     });
 
     it("User can send an invite to a user", () => {
